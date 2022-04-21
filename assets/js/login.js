c615c658cc19e91ea6c3a61037be2633a9deb11e
levana <levanaya@163.com>
2022-04-21 17:06:07 +0800

    完成了登录和注册的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,8 +1,13 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
+$(function(){
+  // 点击去注册账号的链接
+  $('#link_reg').on('click',function(){
     $('.login-box').hide()
     $('.reg-box').show()
+  })
+  // 点击去登录的链接
+  $('#link_login').on('click',function(){
+    $('.login-box').show()
+    $('.reg-box').hide()
   })
 
   // 点击“去登录”的链接
@@ -40,17 +45,17 @@ $(function() {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
     }
-    $.post('/api/reguser', data, function(res) {
-      if (res.status !== 0) {
+
+    $.post('/api/reguser',data,function(res){
+      if(res.status !==0){
         return layer.msg(res.message)
       }
       layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
       $('#link_login').click()
     })
   })
 
-  // 监听登录表单的提交事件
+        // 监听登录表单的提交事件
   $('#form_login').submit(function(e) {
     // 阻止默认提交行为
     e.preventDefault()
@@ -64,6 +69,7 @@ $(function() {
           return layer.msg('登录失败！')
         }
         layer.msg('登录成功！')
+        // console.log(res.token)
         // 将登录成功得到的 token 字符串，保存到 localStorage 中
         localStorage.setItem('token', res.token)
         // 跳转到后台主页
